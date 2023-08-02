445daf5764c04ba295be4d6129d8ce0974c70202
natalinacuna <natalinacuna@gmail.com>
2023-08-02 14:06:33 -0300

    Subiendo primer commit

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,37 @@
+const Paciente = () => {
+  return (
+    <div className='m-3 bg-white shadow-md px-5 py-10 rounded-xl'>
+    <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre: {''}
+      <span className='font-normal normal-case'>
+        Hook
+      </span>
+    </p>
+
+    <p className='font-bold mb-3 text-gray-700 uppercase'>Propietario: {''}
+      <span className='font-normal normal-case'>
+        Naty
+      </span>
+    </p>
+
+    <p className='font-bold mb-3 text-gray-700 uppercase'>Email: {''}
+      <span className='font-normal normal-case'>
+        correo@gmail.com
+      </span>
+    </p>
+
+    <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha de alta: {''}
+      <span className='font-normal normal-case'>
+        10/12/2022
+      </span>
+    </p>
+
+    <p className='font-bold mb-3 text-gray-700 uppercase'>Síntomas: {''}
+      <span className='font-normal normal-case'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla illum iste mollitia, cumque adipisci error sint similique sequi ipsa dolor, a qui porro, obcaecati laboriosam. Dolorum id magni similique totam.
+      </span>
+    </p>
+  </div>
+  )
+}
+
+export default Paciente
