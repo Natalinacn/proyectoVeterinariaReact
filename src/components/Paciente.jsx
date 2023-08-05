a366ac9a55aee09ead0944552f37c8bfcabac6ae
natalinacuna <natalinacuna@gmail.com>
2023-08-05 16:28:20 -0300

    Terminado proyecto

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,37 +1,51 @@
-const Paciente = () => {
-  return (
-    <div className='mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl'>
-    <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre: {''}
-      <span className='font-normal normal-case'>
-        Hook
-      </span>
-    </p>
+const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
+    const { nombre, propietario, email, fecha, sintomas, id } = paciente
 
-    <p className='font-bold mb-3 text-gray-700 uppercase'>Propietario: {''}
-      <span className='font-normal normal-case'>
-        Naty
-      </span>
-    </p>
+    const handleEliminar = () => {
+        const respuesta = confirm('Deseas eliminar este paciente?');
 
-    <p className='font-bold mb-3 text-gray-700 uppercase'>Email: {''}
-      <span className='font-normal normal-case'>
-        correo@gmail.com
-      </span>
-    </p>
+        if(respuesta) {
+            eliminarPaciente(id)
+        }
+    }
 
-    <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha de alta: {''}
-      <span className='font-normal normal-case'>
-        10/12/2022
-      </span>
-    </p>
+    return (
+        <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+                <span className="font-normal normal-case">{nombre}</span>
+            </p>
 
-    <p className='font-bold mb-3 text-gray-700 uppercase'>Síntomas: {''}
-      <span className='font-normal normal-case'>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla illum iste mollitia, cumque adipisci error sint similique sequi ipsa dolor, a qui porro, obcaecati laboriosam. Dolorum id magni similique totam.
-      </span>
-    </p>
-  </div>
-  )
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+                <span className="font-normal normal-case">{propietario}</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+                <span className="font-normal normal-case">{email}</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+                <span className="font-normal normal-case">{fecha}</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+                <span className="font-normal normal-case">{sintomas}</span>
+            </p>
+
+            <div className="flex justify-between mt-10">
+                <button 
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                    onClick={() => setPaciente(paciente)}
+                >Editar</button>
+
+                <button 
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                    onClick={handleEliminar}
+                >Eliminar</button>
+            </div>
+        </div>
+    )
 }
 
 export default Paciente
